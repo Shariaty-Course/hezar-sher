b83c16d76cc3c4854daf0bb14f715c2f3155af07
Nafas Mansouri <nafasmansourii13799@gmail.com>
2022-01-06 15:37:35 +0330

    chore: adds some assets, fixes routing and styles

--- src/router.js
+++ src/router.js
@@ -1,12 +1,14 @@
-import { Switch, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "src/pages/home";
 
 function Router() {
-    <Switch>
-        <Route exact path="/">
-            <Home />
-        </Route>
-    </Switch>;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default Router;
