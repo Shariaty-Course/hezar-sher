58fec2ec4c798b6931ea166008ca5654bbd5674a
Nafas Mansouri <nafasmansourii13799@gmail.com>
2022-01-07 19:28:11 +0330

    feat: adds the faal component

---
+++ src/components/faal/index.js
@@ -0,0 +1,48 @@
+import styles from "./faal.module.css";
+import Button from "src/components/button";
+
+function Faal(props) {
+    const { title, couplets, audioURL, interpretation, onBackButtonClick } =
+        props;
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.poemContainer}>
+                <p className={styles.title}>{title}:</p>
+                <div className={styles.poem}>
+                    {couplets.map((couplet, coupletIndex) => {
+                        return (
+                            <div
+                                key={`couplet-${coupletIndex}`}
+                                className={styles.couplet}
+                            >
+                                {couplet.map((verse, verseIndex) => {
+                                    return (
+                                        <div
+                                            key={`verse-${coupletIndex}-${verseIndex}`}
+                                            className={styles.verse}
+                                        >
+                                            {verse}
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                        );
+                    })}
+                </div>
+                <audio src={audioURL} controls></audio>
+            </div>
+            <div className={styles.interpretationContainer}>
+                <p className={styles.title}>ای صاحب فال:</p>
+                <p className={styles.interpretation}>{interpretation}</p>
+            </div>
+            <Button
+                title="بازگشت به صفحه اصلی"
+                type="secondary"
+                onClick={onBackButtonClick}
+            />
+        </div>
+    );
+}
+
+export default Faal;
