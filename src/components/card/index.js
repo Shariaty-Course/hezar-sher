865e79fbafa346181eb48b54b35453f47081d3b3
Nafas Mansouri <nafasmansourii13799@gmail.com>
2022-01-08 22:45:03 +0330

    chore: enhances the card component style and links

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,13 +1,16 @@
+import { Link } from "react-router-dom";
 import styles from "./card.module.css";
 
 function Card(props) {
-    const { onClick, imageURL, title } = props;
+    const { link, imageURL, title } = props;
 
     return (
-        <div onClick={onClick} className={styles.container}>
-            <img src={imageURL} alt={title} />
-            <p>{title}</p>
-        </div>
+        <Link to={link} target="_blank">
+            <div className={styles.container}>
+                <img src={imageURL} alt={title} />
+                <p>{title}</p>
+            </div>
+        </Link>
     );
 }
 
